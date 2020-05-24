7b5a22f6bbc20d9c279b895457e476c4d93f3319
curioustechizen <curioustechizen@users.noreply.github.com>
2020-05-24 22:38:03 +0200

    Fixed encapsulation of uiState and currentState.
    These fields were publicly settable. Fixed it to make only their getters public and make the setters private.
    
    Fixes #38

--- presentation/lib/src/base_view_model.dart
+++ presentation/lib/src/base_view_model.dart
@@ -2,16 +2,19 @@ import 'package:flutter/foundation.dart';
 import 'package:rxdart/rxdart.dart';
 
 abstract class BaseViewModel<S, A> {
-  @protected S currentState;
-  BehaviorSubject<S> uiState; //TODO: This needs to be final but we cannot use non-static methods in constructor. Find idiomatic Dart way to achieve this
+  S _currentState;
+  @protected S get currentState  => _currentState;
+  
+  BehaviorSubject<S> _uiState; //TODO: This needs to be final but we cannot use non-static methods in constructor. Find idiomatic Dart way to achieve this
+  BehaviorSubject<S> get uiState => _uiState;
 
   final BehaviorSubject<A> _actions = BehaviorSubject<A>(); //TODO: can we make this a simple Stream instead of BehaviourSubject?
 
   @protected Function(A) actionProcessor;
   
   BaseViewModel({@required S initialState}): 
-        this.currentState = initialState {
-    uiState = BehaviorSubject.seeded(
+        this._currentState = initialState {
+    _uiState = BehaviorSubject.seeded(
         initialState,
         onListen: onInit,
         onCancel: onDispose);
@@ -22,9 +25,9 @@ abstract class BaseViewModel<S, A> {
 
   @protected void emit(S state) {
     //TODO: Use streams here instead so that we can use debounce, distinct etc
-    final distinct = currentState != state;
-    currentState = state;
-    if(distinct && !uiState.isClosed) uiState.sink.add(state);
+    final distinct = _currentState != state;
+    _currentState = state;
+    if(distinct && !_uiState.isClosed) _uiState.sink.add(state);
   }
 
   void dispatchAction(A action) {
